60fb43f59ac01b1490e89d34871ccbf7cbe96626
EzequisevlA <ezequielmfsa@hotmail.com>
2025-12-29 13:21:39 +0000

    adicionando static root

--- mainproject/settings.py
+++ mainproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,7 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["44.251.238.113"]
 
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Application definition
 
 INSTALLED_APPS = [
