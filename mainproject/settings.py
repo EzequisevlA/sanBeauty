0ca79f307daa3797ee9e034484ee1d85ea143953
EzequisevlA <ezequielmfsa@hotmail.com>
2025-12-29 13:03:36 +0000

    removendo autorização do allowed hosts

--- mainproject/settings.py
+++ mainproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4v_p0k92u@dkinq@)otbgm=2yfrzhug+o5!5chths2pqb*plzr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['44.251.238.113']
+ALLOWED_HOSTS = []
 
 
 # Application definition
